f2aa91a8d5066b6b3ea855b07f7e0f1cddeee81c
git12nazneen <lipinazneen65@gmail.com>
2024-03-04 00:16:34 +0600

    responsive done

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 const postContainer = document.getElementById('allPosts')
 const latestContainer = document.getElementById('card-container');
-
-
 const fetchCatagories = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
     const data = await res.json();
-    // console.log(data.posts)
     const allposts = data.posts;
     // console.log(allposts)
 }
 const displayPost = (allpost) =>{
     allpost.forEach(post =>{
-        // console.log(post)
         const div = document.createElement('posts');
         div.innerHTML = `
         <div class="hero p-2 rounded-lg border bg-base-200 my-5">
-                    <div class="hero-content flex-col lg:flex-row">
-                        <button class="btn btn-ghost ">
+                    <div class="hero-content flex-col lg:flex-row ">
+                        <button class="btn btn-ghost">
                             <div class="indicator">
-                                <img src="${post.image}" class="w-14 rounded-lg " />
+                                <img src="${post.image}" class="w-16 rounded-lg " />
                                 <span class="badge indicator-item" style="background-color: ${post.isActive ? 'green' : 'red'};"></span>
-          
                                 </div>
                           </button>
                     <div>
@@ -37,7 +32,7 @@ const displayPost = (allpost) =>{
                                     <h6 class="text-gray-600 pr-4"><i class="fa-regular fa-message"></i> ${post.comment_count}</h6>
                                     <h6 class="text-gray-600 pr-4"><i class="fa-thin fa-eye"></i>${post.view_count}</h6>
                                     <h6 class="text-gray-600 pr-4"><i class="fa-regular fa-clock"></i> <span>${post.posted_time}</span>min</h6>
-                                    <div onClick="mailClick('${post.title.replace(/'/g, '@')}', '${post?.view_count}')" class=" bg-green-500 pt-1 pb-1 pl-2 pr-2 ml-56 rounded-full"><i class="fa-solid fa-message"></i></div>  
+                                    <div onClick="mailClick('${post.title.replace(/'/g, '@')}', '${post?.view_count}')" class=" bg-green-500 pt-1 pb-1 pl-2 pr-2 lg:ml-56 rounded-full"><i class="fa-solid fa-message"></i></div>  
                                 </div> 
                            </div>
                       </div>
@@ -97,7 +92,6 @@ const mailClick = (title , visible) =>{
     // Append the new element to a parent element
     const parentElement = document.getElementById('parent-element');
     parentElement.appendChild(newElement);
-
 // mark count
 const markReadSpan = document.getElementById('markRead');
 const currentRead = markReadSpan.innerText;
@@ -105,41 +99,33 @@ const currentMark = parseInt(currentRead);
 const currentVisible = currentMark + 1;
 markReadSpan.innerText = currentVisible;
 }
-
-
+// latest post
 const latestPost = async() =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
     const data = await res.json();
-    // console.log(data)
     const latest = data;
     displayLatestPost(latest)
 }
-
 const displayLatestPost =(latest) =>{
-    console.log(latest);
-
     latest.forEach(blogs =>{
         console.log(blogs)
-       
         const latestCard = document.createElement('div')
-        
         latestCard.innerHTML = `
         <div class=" grid grid-cols-1 lg:grid-cols-3 gap-6">
-      <div class="card card-compact w-96 bg-base-200 shadow-xl ">
-             
-        <figure><img class="p-5" src="${blogs.cover_image}" alt="Shoes" /></figure>
+      <div class="card card-compact w-96 bg-base-200 shadow-xl ">     
+        <figure><img class="p-8" src="${blogs.cover_image}" alt="Shoes" /></figure>
         <div class="card-body">
           <div class="">
             <h1><span class="mr-2"><i class="fa-solid fa-calendar-days"></i></span>${blogs.author?.posted_date ? blogs.author?.posted_date : 'no date published'}</h1>
           </div>
-          <h2 class="card-title font-bold">${blogs.title}</h2>
-          <p>${blogs.description}</p>
+          <h2 class="card-title font-extrabold text-xl">${blogs.title}</h2>
+          <p class="text-sm pb-5">${blogs.description}</p>
           <div class="flex gap-2">
            <div class=" w-10">
-            <img class="rounded-full" src="${blogs.profile_image}" alt="">
+            <img class="rounded-full mr-5" src="${blogs.profile_image}" alt="">
            </div>
            <div>
-            <h1>${blogs.author.name}</h1>
+            <h1 class="text-sm font-extrabold">${blogs.author.name}</h1>
             <h2>${blogs.author?.designation ? blogs.author?.designation : 'Unknown'}</h2>
           </div>
           </div>
@@ -150,11 +136,6 @@ const displayLatestPost =(latest) =>{
         latestContainer.appendChild(latestCard)
     })
 }
-
-
 latestPost()
 fetchDataCatagories()
-
 fetchCatagories()
-
-// ${data.author?.designation ? data.author?.designation : 'Unknown'}
